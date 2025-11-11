84cff4ffd8a74ddc201fb65d573863f53e4441a0
cshirley14 <cShirley14@users.noreply.github.com>
2025-11-11 08:44:14 -0500

    conf: gnome settings

--- home/dot-files/gnome/dconf.nix
+++ home/dot-files/gnome/dconf.nix
@@ -12,6 +12,17 @@
       gtk-theme = "Nordic";
     };
 
+    # Power management - shutdown instead of suspend
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "shutdown";
+      sleep-inactive-battery-type = "shutdown";
+    };
+
+    # Disable auto-login to require login after shutdown
+    "org/gnome/login-screen" = {
+      enable-automatic-login = false;
+    };
+
     # "org/gnome/settings-daemon/plugin/media-keys/custom-keybindings/custom0" = {
     #   name = "emote";
     #   command = "emote";
